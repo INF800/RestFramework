ca2946b991ad714f007fdaf786dc7f36a919cf1c
Asapanna Rakesh <rakeshark22@gmail.com>
2020-05-26 13:53:54 +0530

    session added

---
+++ FastAPI/004-session-cookies-token-auth/README.py
@@ -0,0 +1,1 @@
+https://medium.com/data-rebels/fastapi-how-to-add-basic-and-cookie-authentication-a45c85ef47d3
---
+++ FastAPI/004-session-cookies-token-auth/main.py
@@ -0,0 +1,81 @@
+from typing import Optional
+import base64
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+import jwt
+from jwt import PyJWTError
+
+from pydantic import BaseModel
+
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2
+from fastapi.security.base import SecurityBase
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.utils import get_openapi
+
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, Response, JSONResponse
+from starlette.requests import Request
+
+
+
+class BasicAuth(SecurityBase):
+    def __init__(self, scheme_name: str = None, auto_error: bool = True):
+        self.scheme_name = scheme_name or self.__class__.__name__
+        self.auto_error = auto_error
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "basic":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                )
+            else:
+                return None
+        return param
+
+
+basic_auth = BasicAuth(auto_error=False)
+
+
+
+@app.get("/login_basic")
+async def login_basic(auth: BasicAuth = Depends(basic_auth)):
+    if not auth:
+        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
+        return response
+
+    try:
+        decoded = base64.b64decode(auth).decode("ascii")
+        username, _, password = decoded.partition(":")
+        user = authenticate_user(fake_users_db, username, password)
+        if not user:
+            raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": username}, expires_delta=access_token_expires
+        )
+
+        token = jsonable_encoder(access_token)
+
+        response = RedirectResponse(url="/docs")
+        response.set_cookie(
+            "Authorization",
+            value=f"Bearer {token}",
+            domain="localtest.me",
+            httponly=True,
+            max_age=1800,
+            expires=1800,
+        )
+        return response
+
+    except:
+        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
+        return response
